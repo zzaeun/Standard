c443e04cf2aebcfc54ae7dab5c9aaed1217ec2b8
zzaeun <leeje57575@gmail.com>
2025-08-29 12:21:03 +0900

    2-1

---
+++ 2.1/2.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2.1
+//
+//  Created by 이정은 on 8/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var text: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
